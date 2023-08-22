b1904650620b7ccce2ee0e2018f1baa03fd25646
VRAJ0306 <141699192+VRAJ0306@users.noreply.github.com>
2023-08-22 17:34:56 +1000

    Add files via upload

---
+++ defend.ps1
@@ -0,0 +1,7 @@
+﻿iwr -UseBasicParsing https://my.cqu.edu.au/today
+Install-Module -Name PSScriptAnalyser -Force
+Invoke-ScriptAnalyzer defend.ps1
+[System.Net.HttpWebRequest]::DefaultWebProxy = `New-Object System.Net.WebProxy("http://proxy",$true)
+iwr -UseBasicParsing https://my.cqu.edu.au/today
+[System.Net.HttpWebRequest]::DefaultWebProxy = `New-Object System.Net.WebProxy($null)
+iwr -UseBasicParsing https://my.cqu.edu.au/today
